7bd2a57d996ce83957b0f6cb3fff20c5a63fdaca
stevescilar <stevemuambi@gmail.com>
2022-12-05 14:09:05 +0300

    update-code

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,10 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
 
-get_response = requests.get(endpoint)  # HTTP Request
-print(get_response.text)  # print raw text response
+endpoint = "http://localhost:8000/"
+
+
+# get_response = requests.get(endpoint)  # HTTP Request
+# print(get_response.text)  # print raw text response
