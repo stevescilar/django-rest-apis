1eec20d42a400f4239b843ed8273d40eee33c67d
stevescilar <stevemuambi@gmail.com>
2022-12-05 14:18:01 +0300

    update-code

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def api_home(request, *args, **kwargs):
+    return JsonResponse({"message": "Hi steve this is your django api response"})
